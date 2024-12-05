635a6c4378202e28604e3e3fed8f85b32296a1bd
Essimbi <essimbideranot@gmail.com>
2024-12-05 02:13:45 +0100

    Feed: Création du middleware AdminMiddleware pour la vérification du role de celui qui envoie la requête. J'ai aussi changé le nom de l'id de la migation countries qui est passé de idCountry à id

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Country\CountryController;
 use App\Http\Controllers\Auth\AuthController;
 
 
